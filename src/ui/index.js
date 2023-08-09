37acd5a5fcfd9cf6f179d1e1d07d77f27bc0d7e5
Amit Rathi <amit.rathi.k@gmail.com>
2023-08-09 12:43:22 -0400

    updates

--- src/ui/index.js
+++ src/ui/index.js
@@ -30,8 +30,6 @@ function router(evt) {
             break;
 
     }
-    // let route = resolveRoute(url);
-
     // route();
     console.log(url);
 };
@@ -52,69 +50,9 @@ function router(evt) {
     const WrapperEl = Wrapper();
     $('.wrapper').append(WrapperEl);
 
-    // const BlogSection = Blog();
-    // $('.container').append(Section('blog','From the blog',false,BlogSection));
-    // const PortfolioSection = Grid();
-    // $('.container').append(Section('portfolio','Portfolio',false, PortfolioSection));
     const ProfileSection = Profile();
     $('.container').append(Section('about', '',true,ProfileSection));
 
-    useFancybox();
-
-
-            // Portfolio fancybox
-            var _player;
-            $('.portfolioFancybox').fancybox({
-                padding: 0,
-                wrapCSS: 'fancybox-portfolio',
-                maxWidth: '795px',
-                maxHeight: '85%',
-                minWidth: '250px',
-                mouseWheel: 'true',
-                scrolling: "no",
-                autoCenter: true,
-                beforeShow: function () {
-                    console.log("here")
-                    // Get current popup
-                    var currentID = $(this.element).attr("href");
-                    console.log(currentID)
-                    var currentPopup = $('.fancybox-portfolio ' + currentID);
-
-                    // Append current popup embed
-                    var currentEmbed = currentPopup.find('.inline-embed');
-                    if (currentEmbed.length > 0) {
-                        var currentEmbedType = currentEmbed.data('embed-type');
-                        var curentEmbedUrl = currentEmbed.data('embed-url');
-
-                        switch (currentEmbedType) {
-                            case "image":
-                                currentEmbed.empty();
-                                currentEmbed.addClass('inline-embed-image');
-                                currentEmbed.append('<img src="' + curentEmbedUrl + '" />');
-                                break;
-                            case "iframe":
-                                currentEmbed.empty();
-                                currentEmbed.addClass('inline-embed-iframe');
-                                currentEmbed.append('<iframe src="' + curentEmbedUrl + '" allowfullscreen></iframe>');
-                                break;
-               
-                        }
-                    }
-                },
-                afterShow: function () {
-                    // Get current popup
-                    var currentID = $(this.element).attr("href");
-                    var currentPopup = $('.fancybox-portfolio ' + currentID);
-
-                    // Make current popup visible with css
-                    currentPopup.addClass('opened');
-                },
-                beforeClose: function () {
-                    // reset player
-                    _player = '';
-                }
-            });
-    
 
     hideSitePreloader();
 
